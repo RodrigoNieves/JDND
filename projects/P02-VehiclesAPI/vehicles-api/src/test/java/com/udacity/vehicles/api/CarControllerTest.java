bc1cf0851bb84b1806a952ed7b4cb0c40cdf0d83
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-12-11 19:39:57 -0800

    Add test for put

--- projects/P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ projects/P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -3,9 +3,7 @@ package com.udacity.vehicles.api;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -130,6 +128,21 @@ public class CarControllerTest {
         mvc.perform(delete("/cars/1"))
                 .andExpect(content().string(""))
         .andExpect(status().isNoContent());
+    }
+
+    /**
+     * Test the put of a single car by ID.
+     * @throws Exception if the update operation fails.
+     */
+    @Test
+    public void putCar() throws  Exception {
+        Car car = getCar();
+        car.setCondition(Condition.NEW);
+        mvc.perform(put(new URI("/cars/1"))
+                .content(json.write(car).getJson())
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
     }
 
     /**

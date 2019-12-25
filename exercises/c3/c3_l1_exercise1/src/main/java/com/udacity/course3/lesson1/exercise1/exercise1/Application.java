934e5774583d5a92175571e1c002bc11a8d37565
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-12-25 10:23:54 -0800

    Add connection to data base

--- exercises/c3/c3_l1_exercise1/src/main/java/com/udacity/course3/lesson1/exercise1/exercise1/Application.java
+++ exercises/c3/c3_l1_exercise1/src/main/java/com/udacity/course3/lesson1/exercise1/exercise1/Application.java
@@ -1,11 +1,21 @@
 package com.udacity.course3.lesson1.exercise1.exercise1;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Application {
 
     public static void main(String[] args) {
         // STEP 1: Create the JDBC URL for JDND-C3 database
-
+        try {
+            try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/JDND-C3?user=root?password=rootpass"))
+        } catch (SQLException ex) {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError" + ex.getErrorCode());
+        }
         // STEP 2: Setup and Run Flyway migration that creates the member table using its Java API
         // https://flywaydb.org/getstarted/firststeps/api#integrating-flyway
         // Note the above link talks about connecting to H2 database, for this exercise, MySQL is used. Adapt the code accordingly.

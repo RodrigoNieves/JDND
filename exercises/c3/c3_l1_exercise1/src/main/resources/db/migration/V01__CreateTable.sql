4d23b9f402b22bfda207a0d61e8c08ba31a9846d
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2019-12-25 10:17:23 -0800

    First draft create member table.

--- exercises/c3/c3_l1_exercise1/src/main/resources/db/migration/V01__CreateTable.sql
+++ exercises/c3/c3_l1_exercise1/src/main/resources/db/migration/V01__CreateTable.sql
@@ -1,1 +1,9 @@
--- write sql to create member table
+CREATE TABLE member (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(1000),
+    last_name VARCHAR(1000),
+    age TINIYINT UNSIGNED,
+    genger TINYTEXT,
+    balance FLOAT(7, 2) DEFAULT 0,
+    CONSTRAINT member_pk PRIMARY_KEY(id)
+);

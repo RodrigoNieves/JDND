d0659d7a920f91e9874a349aa8dbea7d54a4dd2f
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-01-12 10:44:27 -0800

    Setting up environment

--- exercises/c3/c3_l4_exercise6/src/main/java/com/udacity/course3/lesson6/exercise4/Application.java
+++ exercises/c3/c3_l4_exercise6/src/main/java/com/udacity/course3/lesson6/exercise4/Application.java
@@ -1,5 +1,9 @@
 package com.udacity.course3.lesson6.exercise4;
 
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -9,18 +13,20 @@ public class Application {
         // Username: course3
         // Password: course3
         // DB: jdnd-c3
-        String uri = "";
+        String uri = "mongodb://course3:course3@localhost:27017//jdnd-c3";
 
         // STEP 2: Create a MongoClient
+        MongoClient mongoClient = (MongoClient) MongoClients.create(uri);
 
 
         // STEP 3: Select the jdnd-c3 database to work with
-
+        MongoDatabase database = mongoClient.getDatabase("jdnd-c3");
 
         // Perform all the steps listed in the exercise
 
 
         // IMPORTANT: Make sure to close the MongoClient at the end so your program exits.
+        mongoClient.close();
     }
 
 }

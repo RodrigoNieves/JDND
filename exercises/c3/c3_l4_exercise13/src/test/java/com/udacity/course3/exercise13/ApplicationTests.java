92a187a8816adced6ecc6187043911749f6a8b97
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-01-25 08:38:53 -0800

    Changes for testing

--- exercises/c3/c3_l4_exercise13/src/test/java/com/udacity/course3/exercise13/ApplicationTests.java
+++ exercises/c3/c3_l4_exercise13/src/test/java/com/udacity/course3/exercise13/ApplicationTests.java
@@ -2,15 +2,21 @@ package com.udacity.course3.exercise13;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@DataMongoTest
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
+    @Autowired MongoTemplate mongoTemplate;
+
     @Test
-    public void contextLoads() {
+    public void test() {
     }
 
 }

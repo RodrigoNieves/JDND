9d30d6a50367c6d4c6c0256a39072a2514daf815
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-01-25 09:25:29 -0800

    Test is not workign

--- exercises/c3/c3_l4_exercise10/src/test/java/com/udacity/course3/exercise10/repository/MemberRepositoryTest.java
+++ exercises/c3/c3_l4_exercise10/src/test/java/com/udacity/course3/exercise10/repository/MemberRepositoryTest.java
@@ -1,19 +1,32 @@
 package com.udacity.course3.exercise10.repository;
 
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+import com.udacity.course3.exercise10.model.Member;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @DataMongoTest
-@EnableAutoConfiguration
+@ExtendWith(SpringExtension.class)
 public class MemberRepositoryTest {
-    @Autowired
-    MongoTemplate mongoTemplate;
+    @Autowired MongoTemplate mongoTemplate;
 
     @Test
-    public void contextLoads() {
+    public void test() {
+        DBObject member = BasicDBObjectBuilder.start()
+                .add("key", "value")
+                .get();
 
+        mongoTemplate.save(member, "members");
+
+        assertThat(mongoTemplate.findAll(DBObject.class,"members").size()).isEqualTo(1);
     }
 }

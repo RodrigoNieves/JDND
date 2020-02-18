ffd363e3e23035bb4b05244e2563545c90cdd683
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-02-18 09:57:45 -0800

    Add test following the lesson video

--- exercises/c4 - testing/starter code/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ exercises/c4 - testing/starter code/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -2,11 +2,38 @@ package com.udacity.examples.Testing;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class HelperTest {
     @Test
     public void test() {
         assertEquals(3,3);
+    }
+
+    @Test
+    public void verify_getCount() {
+        List<String> empNames = Arrays.asList("Rodrigo", "Santiago");
+        long actual = Helper.getCount(empNames);
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void verify_getStats() {
+        List<Integer> yrsOfExperience = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
+        List<Integer> expectEquals = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
+        IntSummaryStatistics stats = Helper.getStats(yrsOfExperience);
+        assertEquals(19,stats.getMax());
+        assertEquals(expectEquals, yrsOfExperience);
+    }
+
+    @Test
+    public void compare_arrays(){
+        int[] yrs = {10,14,2};
+        int[] expected = {10,14,2};
+        assertArrayEquals(expected,yrs);
     }
 }

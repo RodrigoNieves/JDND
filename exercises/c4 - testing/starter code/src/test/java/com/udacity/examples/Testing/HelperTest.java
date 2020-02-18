d5730a7520db093e88f22df07be0bdbdea47bb46
Rodrigo Ruben Santiago Nieves <nievesnievesnieves@gmail.com>
2020-02-18 10:03:56 -0800

    Copy solution

--- exercises/c4 - testing/starter code/src/test/java/com/udacity/examples/Testing/HelperTest.java
+++ exercises/c4 - testing/starter code/src/test/java/com/udacity/examples/Testing/HelperTest.java
@@ -1,6 +1,6 @@
 package com.udacity.examples.Testing;
 
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.Arrays;
 import java.util.IntSummaryStatistics;
@@ -9,38 +9,110 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class HelperTest {
+
     @Test
     public void test() {
-        assertEquals(3,3);
+        assertEquals("test", "test1");
     }
 
     @Test
-    public void verify_getCount() {
-        List<String> empNames = Arrays.asList("Rodrigo", "Santiago");
-        long actual = Helper.getCount(empNames);
-        assertEquals(2, actual);
+    public void test1() {
+        assertEquals("test", "test");
     }
 
-    @Test
-    public void verify_getStats() {
-        List<Integer> yrsOfExperience = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
-        List<Integer> expectEquals = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
-        IntSummaryStatistics stats = Helper.getStats(yrsOfExperience);
-        assertEquals(19,stats.getMax());
-        assertEquals(expectEquals, yrsOfExperience);
+    /**
+     * Need to be void and public
+     * @return
+     *
+     * @Test
+    private String test2() {
+    return "";
     }
+     */
 
     @Test
-    public void compare_arrays(){
-        int[] yrs = {10,14,2};
-        int[] expected = {10,14,2};
-        assertArrayEquals(expected,yrs);
-    }
-
-    @Test
-    public void verify_getMergedList() {
+    public void validate_getCount() {
         List<String> empNames = Arrays.asList("sareeta", "", "john","");
-        String actual = Helper.getMergedList(empNames);
-        assertEquals("sareeta, john",actual);
+        assertEquals(2, Helper.getCount(empNames));
     }
+
+    @Test
+    public void validate_3lengthString() {
+        List<String> empNames = Arrays.asList("sareeta", "", "Jeff","sam");
+        assertEquals(2, Helper.getStringsOfLength3(empNames));
+    }
+
+    /*
+     * Part 2
+     * why not to use test in the name?
+     * Because annotations s
+     */
+
+    @Test
+    public void verify_list_is_squared(){
+        List<Integer> yrsOfExperience = Arrays.asList(13,4,15,6,17,8,19,1,2,3);
+        List<Integer> expected = Arrays.asList(169, 16, 225, 36, 289, 64, 361, 1, 4, 9);
+        assertEquals(expected, Helper.getSquareList(yrsOfExperience));
+    }
+
+
+    /**
+     * assertFail vs asserTrue
+     */
+
+
+    @Before
+    public void init() {
+        System.out.println("init executed");
+
+    }
+
+    @After
+    public void teardown() {
+        System.out.println("teardown executed");
+    }
+
+    @Test
+    public void verify_merged_list(){
+        List<String> empNames = Arrays.asList("sareeta", "", "john","");
+        assertEquals("sareeta, john", Helper.getMergedList(empNames));
+    }
+
+    @Test
+    public void verify_getMax(){
+        List<Integer> empLevel = Arrays.asList(3,3,3,5,7,2,2,5,7,5);
+        assertEquals(7, Helper.getStats(empLevel).getMax());
+    }
+    /**
+     * this should be public static
+     */
+
+    @BeforeClass
+    public static void initClass() {
+        System.out.println("init Class executed");
+
+    }
+
+
+    @AfterClass
+    public static void teardownclass() {
+        System.out.println("teardown Class executed");
+    }
+
+
+    /**
+     *assertArrayEquals
+     *How should you debug this?
+     */
+
+    @Test
+    public void verify_ArrayListTest(){
+        int[] yrsOfExperience = {13,4,15,6,17,8,19,1,2,3};
+        int[] expected = {13,4,15,6,17,8,19,1,2,3};
+        assertArrayEquals(expected, yrsOfExperience);
+    }
+
+    /**
+     * parameterized test next class
+     */
 }
